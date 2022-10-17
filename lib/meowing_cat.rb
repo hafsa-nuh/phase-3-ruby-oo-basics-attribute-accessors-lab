c6807955257f44c742860d806bc1f2f8872b06ed
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-17 23:01:27 +0300

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,22 @@
-## code your solution here. 
+# ## code your solution here. 
+# class Cat
+#     attr_accessor :name
+#     def name=(name)
+#         @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+# end
+# sound = Cat.new
+# sound.name = "meow!"
+# puts sound.name
+
+class Cat
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+end
